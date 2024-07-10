1cc57b9108729586588239748b8dc1f9ecf6ad34
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-10 16:05:53 -0700

    Updated Code of Node Js

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import http, { IncomingMessage, ServerResponse } from "http"
+import *as url from "url";
 
 const hostname: string = 'localhost';
 const port: number = 5000;
 
-http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.statusCode = 200;
-    res.setHeader('content-type', 'text/plain')
-    res.write('First Server of Node Js');
+http.createServer((request: IncomingMessage, response: ServerResponse) => {
+    response.statusCode = 200;
+    // let _url = request.url;
+    // let parsedUrl = url.parse(_url!, true);
+    // let queryParms = parsedUrl.query;
+    // let filterQuery = queryParms.value
+
+
+    let _url = request.url;
+    let queryParms = url.parse(_url!).query;
+    let filterQuery = queryParms?.split("=").pop()!.replaceAll("%27", " ").replaceAll("%20", ' ').replaceAll("%22", " ");
+    let path = url.parse(_url!).pathname;
+
+    if (filterQuery === undefined) {
+        filterQuery = 'No Query Found'
+    }
     
-    res.end()
+    response.setHeader('content-type', 'text/html')
+    response.write(`<h1>Shahwaiz First Server of Node JS</h1> <br> <h2>${filterQuery}</h2><br><h3>${path}</h3>`);
+    
+    response.end()
 }).listen(port, hostname, () => {
     console.log('Server Is Running');
     console.log(`http://${hostname}:${port}`)
